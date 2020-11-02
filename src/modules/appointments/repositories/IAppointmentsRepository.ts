1a5a7cc48ea5993e3a94fc82eb2343d43cd1dbd9
marcos <marcos.rib.sousa@gmail.com>
2020-11-02 17:45:10 -0300

    Adjust apointment

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -5,7 +5,7 @@ import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 
 export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentsDTO): Promise<Appointment>;
-  findByDate(Date: Date): Promise<Appointment | undefined>;
+  findByDate(Date: Date, provider_id: string): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(
     data: IFindAllInMonthFromProviderDTO,
   ): Promise<Appointment[]>;
